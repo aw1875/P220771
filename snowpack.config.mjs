b99b9617226e6f6904c01643059ee6b84045a766
aw1875 <aw1875@rit.edu>
2022-10-07 18:12:14 -0400

    Pushing code

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -7,17 +7,12 @@ export default {
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
-    [
-      '@snowpack/plugin-typescript',
-      {
-        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
-      },
-    ],
+    '@snowpack/plugin-postcss',
+    ['@snowpack/plugin-typescript', { tsc: 'tsc' }],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { match: 'routes', src: '.*', dest: '/index.html' },
   ],
   optimize: {
     /* Example: Bundle your final build: */
@@ -27,9 +22,12 @@ export default {
     /* ... */
   },
   devOptions: {
-    /* ... */
+    tailwindConfig: './tailwind.config.js',
+    port: 3000,
   },
   buildOptions: {
-    /* ... */
+    soureceMaps: true,
+    /* Auto inject React into all pages */
+    jsxInject: "import React from 'react'",
   },
 };
